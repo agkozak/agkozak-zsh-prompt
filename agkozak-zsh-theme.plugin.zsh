eeadf21fce6b7f6e3ccd6ab93fc9c9db67cb9527
Alexandros Kozak <git-agkozak@sneakemail.com>
2017-03-28 23:10:03 -0700

    oh-my-zsh code style

--- agkozak-zsh-theme.plugin.zsh
+++ agkozak-zsh-theme.plugin.zsh
@@ -14,27 +14,27 @@
 setopt PROMPT_SUBST
 
 # Get current branch in git repository
-function parse_git_branch {
-  BRANCH=$( git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/' )
-  if [[ ! "${BRANCH}" == "" ]]
+_parse_git_branch() {
+  local git_branch=$( git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/' )
+  if [[ ! "${git_branch}" == "" ]]
   then
-    STAT=$( parse_git_dirty )
-    echo "(${BRANCH}${STAT}) "
+    local git_status=$( _parse_git_dirty )
+    echo "(${git_branch}${git_status}) "
   else
     echo ""
   fi
 }
 
 # Get current status of git repository
-function parse_git_dirty {
-  git_status=$( git status 2>&1 | tee )
-  dirty=$( echo -n "${git_status}" 2> /dev/null | grep "modified:" &> /dev/null; echo "$?" )
-  untracked=$( echo -n "${git_status}" 2> /dev/null | grep "Untracked files" &> /dev/null; echo "$?" )
-  ahead=$( echo -n "${git_status}" 2> /dev/null | grep "Your branch is ahead of" &> /dev/null; echo "$?" )
-  newfile=$( echo -n "${git_status}" 2> /dev/null | grep "new file:" &> /dev/null; echo "$?" )
-  renamed=$( echo -n "${git_status}" 2> /dev/null | grep "renamed:" &> /dev/null; echo "$?" )
-  deleted=$( echo -n "${git_status}" 2> /dev/null | grep "deleted:" &> /dev/null; echo "$?" )
-  bits=''
+_parse_git_dirty() {
+  local git_status=$( git status 2>&1 | tee )
+  local dirty=$( echo -n "${git_status}" 2> /dev/null | grep "modified:" &> /dev/null; echo "$?" )
+  local untracked=$( echo -n "${git_status}" 2> /dev/null | grep "Untracked files" &> /dev/null; echo "$?" )
+  local ahead=$( echo -n "${git_status}" 2> /dev/null | grep "Your branch is ahead of" &> /dev/null; echo "$?" )
+  local newfile=$( echo -n "${git_status}" 2> /dev/null | grep "new file:" &> /dev/null; echo "$?" )
+  local renamed=$( echo -n "${git_status}" 2> /dev/null | grep "renamed:" &> /dev/null; echo "$?" )
+  local deleted=$( echo -n "${git_status}" 2> /dev/null | grep "deleted:" &> /dev/null; echo "$?" )
+  local bits=''
   [[ "${renamed}" == "0" ]] && bits=">${bits}"
   [[ "${ahead}" == "0" ]] && bits="*${bits}"
   [[ "${newfile}" == "0" ]] && bits="+${bits}"
@@ -58,13 +58,13 @@ MODE_INDICATOR="%{$fg_bold[red]%}"
 
 # If the vi-mode plugin is not loaded, vi_mode_prompt_info() should do nothing
 if ! whence -w vi_mode_prompt_info > /dev/null 2>&1; then
-  function vi_mode_prompt_info {
+	vi_mode_prompt_info() {
     echo ''
   }
 fi
 
 # The main prompt
-PROMPT='%{$fg_bold[green]%}%n@%m%{$reset_color%} %{$fg_bold[blue]%}%(3~|%2~|%~)%{$reset_color%} %{$fg[yellow]%}$( parse_git_branch )%{$reset_color%}$( vi_mode_prompt_info )%%%{$reset_color%} '
+PROMPT='%{$fg_bold[green]%}%n@%m%{$reset_color%} %{$fg_bold[blue]%}%(3~|%2~|%~)%{$reset_color%} %{$fg[yellow]%}$( _parse_git_branch )%{$reset_color%}$( vi_mode_prompt_info )%%%{$reset_color%} '
 
 # The right prompt will show the exit code if it is not zero.
 RPS1="%(?..%{$fg_bold[red]%}(%?%)%{$reset_color%})"

557044dc41b97c860c2603d8e11e7a18a1ab92d3
Alexandros Kozak <git-agkozak@sneakemail.com>
2018-05-10 08:40:55 -0700

    Use 3 prompt glitches to fix prompt for zsh 5.5

--- agkozak-zsh-theme.plugin.zsh
+++ agkozak-zsh-theme.plugin.zsh
@@ -397,7 +397,6 @@ _agkozak_precmd() {
     'usr1') _agkozak_usr1_async ;;
     *) psvar[3]="$(_agkozak_branch_status)" ;;
   esac
-
 }
 
 ############################################################
@@ -432,7 +431,7 @@ agkozak_zsh_theme() {
   # When the user is a superuser, the username and hostname are
   # displayed in reverse video
   if _agkozak_has_colors; then
-    PS1='%(?..%B%F{red}(%?%)%f%b )%(!.%S%B.%B%F{green})%n%1v%(!.%b%s.%f%b) %B%F{blue}%2v%f%b $(_agkozak_vi_mode_indicator) '
+    PS1='%(?..%B%F{red}(%?%)%f%b )%(!.%S%B.%B%F{green})%n%1v%(!.%b%s.%f%b) %B%F{blue}%2v%f%b %{$(_agkozak_vi_mode_indicator)%3G%} '
     RPS1='%F{yellow}%3v%f'
   else
     PS1='%(?..(%?%) )%(!.%S.)%n%1v%(!.%s.) %2v $(_agkozak_vi_mode_indicator) '

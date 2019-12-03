0e3eeef10751bebe1595457c67c29b2c647ce450
Alexandros Kozák <git-agkozak@sneakemail.com>
2019-12-02 22:06:06 -0800

    Better test for glitch fix compliance

--- agkozak-zsh-prompt.plugin.zsh
+++ agkozak-zsh-prompt.plugin.zsh
@@ -872,7 +872,8 @@ _agkozak_precmd() {
     { { (( AGKOZAK_MULTILINE )) && (( ! ${AGKOZAK_LEFT_PROMPT_ONLY} )); } \
     || (( $+AGKOZAK_CUSTOM_PROMPT )); } \
     && [[ ${AGKOZAK[PROMPT]} == *(\$\{AGKOZAK_PROMPT_WHITESPACE\}|$'\n')* ]] \
-    && [[ ${AGKOZAK[PROMPT]} != *%3v* ]] && [[ ${AGKOZAK[PROMPT]} != *\%\?* ]] \
+    && [[ ${AGKOZAK[PROMPT]} != *%3v*$'\n'* ]] \
+    && [[ ${AGKOZAK[PROMPT]} != *\%\?*$'\n'* ]] \
     && [[ -z ${INSIDE_EMACS} ]]; then
 
     print -Pnz -- ${AGKOZAK[PROMPT]}
